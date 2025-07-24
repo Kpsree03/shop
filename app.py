313248fb5c294204fcd5e43cf194e6a5ec5cede3
Kpsree03 <konerupoojasree@gmail.com>
2025-07-24 11:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ if __name__ == '__main__':
     # Create upload folder if it doesn't exist
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
